fe59e2d1ae71674775f72a83be8554f9eacdf1d5
JordanBurdick <31458582+JordanBurdick@users.noreply.github.com>
2018-05-11 14:09:16 -0400

    Add files via upload

---
+++ Lab09JordanBurdick/Lab09JordanBurdick/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09JordanBurdick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath MJ = new MyMath();
+
+            MJ.Divide(12, 3);
+            Console.WriteLine(MJ.getResult());
+            MJ.Add(2, 2);
+            Console.WriteLine(MJ.getResult());
+            MJ.Subtract(100, 50);
+            Console.WriteLine(MJ.getResult());
+            MJ.Multiply(20, 4);
+            Console.WriteLine(MJ.getResult());
+
+
+
+            //pls
+        }
+    }
+}
